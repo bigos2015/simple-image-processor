d9c530aa53237b6d6a613eba15f6fcd238dd7663
Michał Stocki <michal.m.stocki@gmail.com>
2016-02-11 10:08:01 +0100

    Nareszcie! Ładowanie obrazka z podanego adresu.

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,5 +1,7 @@
 var form = document.getElementById('image-selector-form');
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-
+    var inputUrl = document.querySelector('input#url');
+    var image = document.querySelector('.image-to-cropp');
+    image.src = inputUrl.value;
 });
