dbc7a5d1b6b8652f088baeec56f2bf49f7da1221
Michał Stocki <michal.m.stocki@gmail.com>
2016-04-28 09:44:42 +0200

    Przesuwanie obszaru kadrującego: zapobieganie przekroczeniu lewej i górnej krawędzi obrazka

--- scripts/cropper.js
+++ scripts/cropper.js
@@ -17,6 +17,20 @@ document.addEventListener('mouseup', function () {
 
 function moveArea(event) {
     var containerPosition = cropperContainer.getBoundingClientRect();
-    cropper.style.left = event.clientX - dragOffsetX - containerPosition.left + 'px';
-    cropper.style.top = event.clientY - dragOffsetY - containerPosition.top + 'px';
+    var cropperX = calculateCropperX(containerPosition, event.clientX);
+    var cropperY = calculateCropperY(containerPosition, event.clientY);
+
+    cropperX = Math.max(cropperX, 0);
+    cropperY = Math.max(cropperY, 0);
+
+    cropper.style.left = cropperX + 'px';
+    cropper.style.top = cropperY + 'px';
+}
+
+function calculateCropperX(containerPosition, mouseX) {
+    return mouseX - dragOffsetX - containerPosition.left;
+}
+
+function calculateCropperY(containerPosition, mouseY) {
+    return mouseY - dragOffsetY - containerPosition.top;
 }
