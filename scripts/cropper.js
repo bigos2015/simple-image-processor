40e292bb4d2940bb9b9f560fe4b8fa939689d2de
Michał Stocki <michal.m.stocki@gmail.com>
2016-05-12 10:17:57 +0200

    Przesuwanie obszaru kadrującego: zapobieganie przekroczeniu dolnej i prawej krawędzi obrazka

--- scripts/cropper.js
+++ scripts/cropper.js
@@ -19,9 +19,12 @@ function moveArea(event) {
     var containerPosition = cropperContainer.getBoundingClientRect();
     var cropperX = calculateCropperX(containerPosition, event.clientX);
     var cropperY = calculateCropperY(containerPosition, event.clientY);
+    var cropperPosition = cropper.getBoundingClientRect();
 
     cropperX = Math.max(cropperX, 0);
     cropperY = Math.max(cropperY, 0);
+    cropperX = Math.min(containerPosition.width - cropperPosition.width, cropperX);
+    cropperY = Math.min(containerPosition.height - cropperPosition.height, cropperY);
 
     cropper.style.left = cropperX + 'px';
     cropper.style.top = cropperY + 'px';
