ef875726cab220a3de22351ac051cb219cfbe046
Michał Stocki <michal.m.stocki@gmail.com>
2016-06-09 10:05:52 +0200

    Resizing right and bottom edge

--- scripts/cropper-resize.js
+++ scripts/cropper-resize.js
@@ -7,6 +7,7 @@ var lbHandle = document.querySelector('.handle.lb');
 
 rbHandle.addEventListener('mousedown', function (event) {
     event.preventDefault();
+    event.stopImmediatePropagation();
     document.addEventListener('mousemove', resizeRight);
     document.addEventListener('mousemove', resizeBottom);
 });
@@ -23,12 +24,16 @@ function resizeTop(event) {
 }
 
 function resizeRight(event) {
-
+    var cropperPosition = cropper.getBoundingClientRect();
+    var newWidht = event.clientX - cropperPosition.left;
+    cropper.style.width = newWidht + 'px';
 }
 
 
 function resizeBottom(event) {
-
+    var cropperPosition = cropper.getBoundingClientRect();
+    var newHeight = event.clientY - cropperPosition.top;
+    cropper.style.height = newHeight + 'px';
 }
 
 
